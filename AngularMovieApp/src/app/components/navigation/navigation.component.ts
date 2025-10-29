b740a200d3c12f7a22482c1873719097fa61b9b6
Musa Gumede <gracegumedem@gmail.com>
2025-10-29 12:31:55 +0200

    added navigation to other pages from favorites component

--- AngularMovieApp/src/app/components/navigation/navigation.component.ts
+++ AngularMovieApp/src/app/components/navigation/navigation.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
+  standalone:false
 })
 export class NavigationComponent {}
